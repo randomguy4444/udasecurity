7fbf0278f3a56f5ed9f17adcc6cbeac8a83d6977
Rex Guo <xiaofei.guo.rex@gmail.com>
2022-11-25 21:04:02 -0800

    added UT

---
+++ cd0384-java-application-deployment-projectstarter/starter/catpoint-parent/securityService/src/test/java/com.udacity.security.service/SecurityServiceTest.java
@@ -0,0 +1,15 @@
+package com.udacity.security.service;
+
+import com.udacity.image.service.ImageService;
+import com.udacity.security.service.SecurityService;
+
+/**
+ * Unit test for SecurityService.
+ */
+class SecurityServiceTest {
+
+    void test() {
+        System.out.println("Unit test");
+    }
+
+}
